0ba4d6cc38db6cae7d0b62e8bc555b255e5abd43
Sayed Navid <navidjan.azimi132@gmail.com>
2025-06-30 15:17:04 +0430

    create is done

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'Hello World';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Hello World #${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
